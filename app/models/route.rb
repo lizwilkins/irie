a0c5c310d9ffb136ca90e0b9dd69139a9f9c0b14
Thomas Hopkins & Michael Kaiser-Nyman <student@epicodus.com>
2013-04-12 11:28:56 -0700

    add validations to route, rename number column

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,5 @@
 class Route < ActiveRecord::Base
+  validates :number, :presence => :true
+  validates :origin, :presence => :true
+  validates :destination, :presence => :true
 end
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe Route do
+  context 'validations' do
+    it {should validate_presence_of :number}
+    it {should validate_presence_of :origin}
+    it {should validate_presence_of :destination}
+  end
 end
