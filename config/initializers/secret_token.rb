c5fa3f557b1157a16888714b22d63fe92a434498
Diane Wilkins & Dorrit Geshuri <dorritgeshuri@gmail.com>
2013-04-12 10:30:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irie::Application.config.secret_token = 'ece631c4fecaab4dc04eed089ed247c3a3c828bb69931effb6e38955595f17e532bd62352ee225ff21204b073dbe11f51bb75f3a8862c63fdc93fb94240f2455'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Irie::Application.config.session_store :cookie_store, key: '_irie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Irie::Application.config.session_store :active_record_store
