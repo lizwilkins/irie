58ba0a497acc5b0636ed5ee9ac4e5701f6a78316
Diane <wilkins.lizzie@gmail.com>
2013-04-13 16:36:12 -0700

    merge and resolve conflicts

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,7 +6,6 @@ FactoryGirl.define do
     path 'express'
   end
 
-FactoryGirl.define do 
   factory :user do
     sequence(:username) {|n| "user#{n}"}
     sequence(:email) {|n| "email#{n}@gmail.com"}
